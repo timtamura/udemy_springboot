1f2c5b77cde213727a0f98d418da66baf5757729
ytamura <ytamura@example.com>
2023-05-12 20:34:28 -0600

    Section 4: Spring Data Rest

---
+++ 04-rest-crud/demo-employee-spring-data-rest/src/main/java/com/example/demo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // No need to write any code!
+}
